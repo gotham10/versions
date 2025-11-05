6f7c644cb51253d29a9d08d80907e9a42d9c7317
gotham10 <notagainbruh1236@outlook.com>
2025-11-05 17:24:42 -0600

    Update v4.lua

--- v4.lua
+++ v4.lua
@@ -1,3 +1,20 @@
+_G.Config = {}
+_G.Config.wl = {"DARK_ENCHANTEDD"}
+_G.Config.SEED_MIN_PRICE = 200
+_G.Config.GEAR_MIN_PRICE = 7500
+_G.Config.AutoSeedBuyer = true
+_G.Config.AutoGearBuyer = true
+_G.Config.AutoSellLimited = true
+_G.Config.AutoSellSecret = true
+_G.Config.AutoSellSecretNames = {"Hacktini Adminini"}
+_G.Config.AutoBrainrot = true
+_G.Config.AutoBrainrotWait = 60
+_G.Config.AutoAcceptGifts = true
+_G.Config.AutoBuyHalloweenCrate = true
+_G.Config.seedRegistryTable = {["Cactus Seed"] = { Plant = "Cactus", Price = 200 }, ["Strawberry Seed"] = { Plant = "Strawberry", Price = 1250 }, ["Pumpkin Seed"] = { Plant = "Pumpkin", Price = 5000 }, ["Sunflower Seed"] = { Plant = "Sunflower", Price = 25000 }, ["Dragon Fruit Seed"] = { Plant = "Dragon Fruit", Price = 100000 }, ["Eggplant Seed"] = { Plant = "Eggplant", Price = 250000 }, ["Watermelon Seed"] = { Plant = "Watermelon", Price = 1000000 }, ["Grape Seed"] = { Plant = "Grape", Price = 2500000 }, ["Cocotank Seed"] = { Plant = "Cocotank", Price = 5000000 }, ["Carnivorous Plant Seed"] = { Plant = "Carnivorous Plant", Price = 25000000 }, ["Mr Carrot Seed"] = { Plant = "Mr Carrot", Price = 50000000 }, ["Tomatrio Seed"] = { Plant = "Tomatrio", Price = 125000000 }, ["Shroombino Seed"] = { Plant = "Shroombino", Price = 200000000 }, ["Mango Seed"] = { Plant = "Mango", Price = 367000000 }, ["King Limone Seed"] = { Plant = "King Limone", Price = 670000000 }, ["Starfruit Seed"] = { Plant = "Starfruit", Price = 750000000 }}
+_G.Config.gearRegistryTable = {["Water Bucket"] = { Price = 7500 }, ["Frost Grenade"] = { Price = 12500 }, ["Banana Gun"] = { Price = 25000 }, ["Frost Blower"] = { Price = 125000 }, ["Carrot Launcher"] = { Price = 500000 }}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gotham10/versions/main/v4.lua"))()
+-- ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 _G.ScriptManager = _G.ScriptManager or { Connections = {}, Threads = {} }
 
 local function cleanup(key)
