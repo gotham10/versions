dd12919d97a92e7e9a151817c58bdaa09021174e
gotham10 <notagainbruh1236@outlook.com>
2025-11-08 17:52:29 -0600

    Create v32.lua

---
+++ runner/v32.lua
@@ -0,0 +1,16 @@
+_G.Config = {}
+_G.Config.wl = {"DARK_ENCHANTEDD","pvbfarmeracc11","pvbfarmeracc12","pvbfarmeracc13","pvbfarmeracc14","pvbfarmeracc15","pvbfarmeracc21","pvbfarmeracc22","pvbfarmeracc23","pvbfarmeracc24","pvbfarmeracc25","pvbfarmeracc26","pvbfarmeracc27","pvbfarmeracc28","pvbfarmeracc29","pvbfarmeracc30"}
+_G.Config.SEED_MIN_PRICE = 200
+_G.Config.GEAR_MIN_PRICE = 7500
+_G.Config.AutoSeedBuyer = true
+_G.Config.AutoGearBuyer = true
+_G.Config.AutoBrainrot = false
+_G.Config.AutoBrainrotWait = 60
+_G.Config.AutoAcceptGifts = false
+_G.Config.AntiAFK = false
+_G.Config.ShowShopTimer = true
+_G.Config.SafeLocation = false
+_G.Config.OPlessLag = false
+_G.Config.seedRegistryTable = {["Cactus Seed"] = { Plant = "Cactus", Price = 200 }, ["Strawberry Seed"] = { Plant = "Strawberry", Price = 1250 }, ["Pumpkin Seed"] = { Plant = "Pumpkin", Price = 5000 }, ["Sunflower Seed"] = { Plant = "Sunflower", Price = 25000 }, ["Dragon Fruit Seed"] = { Plant = "Dragon Fruit", Price = 100000 }, ["Eggplant Seed"] = { Plant = "Eggplant", Price = 250000 }, ["Watermelon Seed"] = { Plant = "Watermelon", Price = 1000000 }, ["Grape Seed"] = { Plant = "Grape", Price = 2500000 }, ["Cocotank Seed"] = { Plant = "Cocotank", Price = 5000000 }, ["Carnivorous Plant Seed"] = { Plant = "Carnivorous Plant", Price = 25000000 }, ["Mr Carrot Seed"] = { Plant = "Mr Carrot", Price = 50000000 }, ["Tomatrio Seed"] = { Plant = "Tomatrio", Price = 125000000 }, ["Shroombino Seed"] = { Plant = "Shroombino", Price = 200000000 }, ["Mango Seed"] = { Plant = "Mango", Price = 367000000 }, ["King Limone Seed"] = { Plant = "King Limone", Price = 670000000 }, ["Starfruit Seed"] = { Plant = "Starfruit", Price = 750000000 }}
+_G.Config.gearRegistryTable = {["Water Bucket"] = { Price = 7500 }, ["Frost Grenade"] = { Price = 12500 }, ["Banana Gun"] = { Price = 25000 }, ["Frost Blower"] = { Price = 125000 }, ["Carrot Launcher"] = { Price = 500000 }}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gotham10/versions/main/v32.lua"))()
